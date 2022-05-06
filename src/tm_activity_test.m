ec0f2e9f631967d46198d7a62d4c50daee0d673c
Nate Sutton <natemsutton@gmail.com>
2022-05-06 13:28:01 -0400

    backup

--- src/tm_activity_test.m
+++ src/tm_activity_test.m
@@ -19,15 +19,18 @@ x_km = 1;
 A_km = 0;%1;%20*200;%4;%2;
 %w_km = 10;
 i_km = 0;
-Vm = 0;%-80;
+Vm = -80;
 pst = 0;
-spk = 1;
+spk = 0;
+fst_spk_km = 10;
 ts=0.5; % time step
-t_total = 1000;
+ts_km=0.025;%.5;%0.025; % time step
+steps=40;%2;%40;
+t_total = 200;%1000;
 simdur = linspace(0,t_total,t_total);
 u_all_cs=[]; u_all_km=[];
 x_all_cs=[]; x_all_km=[];
-i_all_cs=[]; i_all_km=[];
+i_all_cs=[]; i_all_km=[]; a_all_km=[];
 
 for t=1:length(simdur)
     % CARLsim's methods
@@ -38,12 +41,41 @@ for t=1:length(simdur)
 	i_cs=i_cs*g_cs;
 
     if mod(t,200)==0
-        spk = 1;
+    %    spk = 1;
     else
-        spk = 0;
+    %    spk = 0;
     end
     
     % Keivan's methods
+    u_km=u_km+((-u_km/tau_u_km)+(cap_u_km*(1-u_km)).*spk);
+    x_km=x_km+(((1-x_km-A_km)/tau_x_km)-u_km.*x_km.*spk);
+    %A_km=A_km+ts*((-i_km/tau_d_km)+u_km.*x_km.*spk);
+    %A_km=A_km+ts*((-i_km/tau_d_km)+u_km.*x_km.*spk);
+    A_km=A_km+((-A_km/tau_d_km)+u_km.*x_km.*spk);
+    %i_km=i_km*g_km;
+    i_km=g_km*A_km*Vm;
+    %i_km=i_km+ts*(g_km*A_km*Vm);
+    %i_km=i_km+ts*(g_km*A_km*Vm);
+    for s=1:length(steps)
+    %    i_km=i_km+ts_km*(g_km*A_km*Vm);       
+    end
+    %deltat = pst-t;
+    %g = g_km*A_km;
+    %i_km=g*exp(deltat/tau_d_km)*Vm;
+
+    if mod((t+fst_spk_km),20)==0
+        spk = 1;
+        pst = t;
+    else
+        spk = 0;
+    end
+    if t == 50
+    %    spk = 1;
+    %    pst = t;
+    else
+    %    spk = 0; 
+    end
+    %{
     deltat = pst-t;
     tau1r = tau_d_km / (tau_d_km-tau_x_km);
     y_ = A_km * exp(deltat/tau_d_km);
@@ -56,37 +88,17 @@ for t=1:length(simdur)
         i_km=g_km*A_km*Vm;
     end
     if 1
-        %{
-        weight=12;
-        g = A_km*weight;
-        g = g + ts*(-g/tau_d_km);
-        g = g + ts*(-g/tau_d_km);
-        i_km=g*A_km*Vm;
-        %}
         g = g_km*A_km;
-
         i_km=g*exp(-deltat/tau_d_km)*Vm;
     end
     if mod(t,20)==0
-        %pst = t;   
-        %Vm = -80;
-        %u_km = 0;
-        %x_km = 1;
-        %A_km = 0;
-        %i_km = i_km - 120;
         pst = t;
-    else
-        %Vm = 0;
     end
-    if t == 50
-    %    u_km = 0;
-    %    x_km = 1;
-    %    A_km = 0;
-    end
-    pst = t;
+    %pst = t;
+    %}
     
 	u_all_cs(end+1)=u_cs;x_all_cs(end+1)=x_cs;i_all_cs(end+1)=i_cs;
-    u_all_km(end+1)=u_km;x_all_km(end+1)=x_km;i_all_km(end+1)=i_km;
+    u_all_km(end+1)=u_km;x_all_km(end+1)=x_km;a_all_km(end+1)=A_km;i_all_km(end+1)=i_km;
 end
 
 if 0
@@ -101,9 +113,12 @@ end
 if 1
 %subplot(2,1,2); 
 figure
-plot(simdur,u_all_km,'-',simdur,x_all_km,'-',simdur,i_all_km,'-','LineWidth',3);
+plot(simdur,u_all_km,'-',simdur,x_all_km,'-',simdur,a_all_km,'-',simdur,i_all_km,'-','LineWidth',3);
+%plot(simdur,u_all_km,'-',simdur,x_all_km,'-',simdur,a_all_km,'-','LineWidth',3);
+%plot(simdur,u_all_km,'-',simdur,x_all_km,'-',simdur,i_all_km,'-','LineWidth',3);
 %plot(simdur,u_all_km,'-',simdur,x_all_km,'-','LineWidth',3);
-legend('u parameter','x parameter','i parameter')
+%plot(simdur,i_all_km,'-','LineWidth',3);
+legend('u parameter','x parameter','a parameter','i parameter')
 title("Keivan Synaptic Current - U:"+cap_u_km+"; t_u:"+tau_u_km+"; t_x:"+tau_x_km+"; t_d:"+tau_d_km+"; g:"+g_km)
 grid on;
 end
